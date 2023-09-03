31b77025628584d5688c4965c1df02677fe9ac9a
Farhan Bukhari <bfarhan03@gmail.com>
2023-09-03 19:54:27 +0530

    self update:

--- oop2.py
+++ oop2.py
@@ -4,15 +4,30 @@ class Computer:
         self.age=28
     def update(self):
         self.age=30
+    def compare(self,other):
+        if self.age==other.age:
+            return True
+        else:
+            return False
+
 
 c1 = Computer()
+c1.age=44
 c2=Computer()
 
 print(c1.age)
 #Will print 28
 
+if(c1.compare(c2)):
+    #c1 becomes self and c2 becomes other
+    #c1 becomes self since c1 is calling it
+    print("They are same")
+else:
+    print("They are different")
+
 c1.update()
 #Now, how will update know which object's update is called? That will be done with the help of self.
+#Current instance is passed
 
 print(c1.age)
 #Will print 30
