fbee7693853bd8f87a2e0561a57a14ad83267c9a
Farhan Bukhari <bfarhan03@gmail.com>
2023-09-03 20:20:24 +0530

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -2,9 +2,20 @@ class A:
     def f1(self):
         print("feature1")
 
+
+#Single inheritance
 class B(A):
     def f2(self):
         print("feature2")
+
+class C:
+    def f3(self):
+        print("feature3")
+
+#Multiple inheritance
+class D(A,C):
+    def f4(self):
+        print("feature3")
 
 a1=A()
 
@@ -14,3 +25,8 @@ b1=B()
 
 b1.f1()
 b1.f2()
+
+d1=D()
+d1.f3()
+d1.f4()
+d1.f1()
